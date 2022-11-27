e7ba9f577eb6df6ac70a53b16aec43a60b8a9c2e
ShamimHosen75 <developer.shamimhosen@gmail.com>
2022-11-27 23:51:36 +0600

    added some changes

--- src/Components/UI/Clock.jsx
+++ src/Components/UI/Clock.jsx
@@ -10,7 +10,7 @@ const Clock = () => {
   let interval;
 
   const countDown = () => {
-    const destination = new Date('Nov 25, 2022').getTime()
+    const destination = new Date('dec 16, 2022').getTime()
 
     interval = setInterval (() =>{
       const now = new Date().getTime()
