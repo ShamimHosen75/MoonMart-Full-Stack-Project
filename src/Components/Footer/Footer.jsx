4f8a6bc5f1ef261e059c52fd76ab880ae47d74cd
ShamimHosen75 <developer.shamimhosen@gmail.com>
2022-12-03 23:45:38 +0600

    added footer section

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,10 +1,37 @@
 import React from 'react';
+import { Col, Container, Row } from 'reactstrap';
+import logo from '../../assets/images/eco-logo.png';
+import './Footer.css';
 
 const Footer = () => {
   return (
-    <div>
-      
-    </div>
+    <footer>
+      <Container>
+        <Row>
+          <Col className='lg-4'>
+           <div className='logo'>
+                <img src={logo} alt="logo" />
+                <div>
+                  <h1>MoonMart</h1>
+                </div>
+            </div>
+
+            <p className='footer-text'>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem nihil odit similique ab enim voluptatum quod reprehenderit unde quae! Nesciunt.
+            </p>
+          </Col>
+          <Col className='lg-3'>
+           
+          </Col>
+          <Col className='lg-2'>
+           
+          </Col>
+          <Col className='lg-3'>
+           
+          </Col>
+        </Row>
+      </Container>
+    </footer>
   );
 };
 
